6d72ed8333829cfa012c4eadf3e05d9a3d7c4550
DrMikeG <github@michaelgibbens.co.uk>
2019-01-29 21:14:36 +0000

    get state or push state is working

--- pynfc-master/src/socketTest.py
+++ pynfc-master/src/socketTest.py
@@ -1,21 +1,32 @@
 from socketIO_client import SocketIO, LoggingNamespace
 
+socketIO = SocketIO('localhost', 3000, LoggingNamespace)
+
 #socketIO = SocketIO('localhost', 3000)
 #socketIO.emit('replaceAndPlay', {"uri":"live_playlists_random_50", "title":"50 random tracks", "service":"live_playlists"})
 
 
 def mute():
-    with SocketIO('localhost', 3000, LoggingNamespace) as socketIO:
-        socketIO.emit('mute', '')
-        socketIO.wait(seconds=1)
-#   socketIO.on('pushState', on_push_state)
+    socketIO.emit('mute', '')
+    socketIO.wait(seconds=1)
+
+def on_push_state(*args):
+    print('state', args)
+    global status
+    status = args[0]['status'].encode('ascii', 'ignore')
+    print status
+
+def getState():
+     socketIO.on('pushState', on_push_state)
+     #get initial state
+     socketIO.emit('getState', '', on_push_state)
+
+#mute()
 
 # get initial state
-#socketIO.emit('getState', '', on_push_state)
+getState()
 
-#try:
-#	socketIO.wait()
-#except KeyboardInterrupt:
-#	pass
-
-mute()
+try:
+	socketIO.wait()
+except KeyboardInterrupt:
+	pass
