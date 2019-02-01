70a3d1f34b507738d89567d0fd6d37a37646df98
DrMikeG <github@michaelgibbens.co.uk>
2019-02-01 07:54:39 +0000

    working around various artists bug

--- pynfc-master/src/socketTest.py
+++ pynfc-master/src/socketTest.py
@@ -47,12 +47,12 @@ def replaceAndPlay():
 	#socketIO.emit('replaceAndPlay', {"uri":"albums://Various%20Artists/Keep%20Calm%20And%20Stay%20Cosy"})
 	#socketIO.emit('replaceAndPlay', {"uri":"albums:///This%20House%20Is%20Not%20For%20Sale"})
 	#socketIO.emit('replaceAndPlay', {"uri":"albums:///Jupiter%20Calling"})
-	#socketIO.emit('replaceAndPlay', {"uri":"albums:///Platinum%20Collection"})
+    #socketIO.emit('replaceAndPlay', {"uri":"albums://Queen//Platinum%20Collection"})
 	#socketIO.emit('replaceAndPlay', {"uri":"albums://Barenaked%20Ladies/Disc%20One%3A%20%20All%20Their%20Greatest%20Hits%201991-2001"})
 	#socketIO.emit('replaceAndPlay', {"uri":"albums:///Swing%20When%20You're%20Winning"})
 	#socketIO.emit('replaceAndPlay', {"uri":"albums://Various%20Artists/100%20Rock%20n%20Roll%20Hits%20%26%20Jukebox%20Classics%20-%20The%20Very%20Best%2050s%20%26%2060s%20Rock%20and%20Roll%20Collection%20from%20the%20Greatest%20Legends"})
-    socketIO.emit('replaceAndPlay', {"uri":"artists://Barenaked%20Ladies/Disc%20One%3A%20%20All%20Their%20Greatest%20Hits%201991-2001"})
-
+    #socketIO.emit('replaceAndPlay', {"uri":"artists://Barenaked%20Ladies/Disc%20One%3A%20%20All%20Their%20Greatest%20Hits%201991-2001"})
+#    socketIO.emit('replaceAndPlay', {"uri":"music-library/USB/Lexar/music/The Corrs/Jupiter Calling"})
 
 def stop():
 	print 'stop'
